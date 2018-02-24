7a4c21a3c9e278597ac30ac659018cdc5b5d4528
Unknown <samhutton@hotmail.co.uk>
2018-02-24 23:54:34 +0000

    Clicks now only call methods on the first frame of mouse down

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -7,20 +7,32 @@ public class InputController : MonoBehaviour {
   private World _world;
   [SerializeField]
   private UIController _ui_controller;
+
+  private bool _mouse_was_down = false;
   
   void Update () {
-		if (Input.GetMouseButtonDown(0)) {
-      Vector3 mouse_position = Input.mousePosition;
-      AbilityButton selected_button;
-      Character selected_character;
-      if (_ui_controller.TrySelectAbilityButton(mouse_position, out selected_button))
+    if (Input.GetMouseButtonDown(0))
+    {
+      if (!_mouse_was_down) 
       {
-        _player.SelectAbility(selected_button.GetAbility());
+        Vector3 mouse_position = Input.mousePosition;
+        AbilityButton selected_button;
+        Character selected_character;
+        if (_ui_controller.TrySelectAbilityButton(mouse_position, out selected_button))
+        {
+          _player.SelectAbility(selected_button.GetAbility());
+        }
+        else if (_world.TrySelectCharacter(mouse_position, out selected_character))
+        {
+          _player.SelectCharacter(selected_character);
+        }
+
+        _mouse_was_down = true;
       }
-      else if (_world.TrySelectCharacter(mouse_position, out selected_character))
-      {
-        _player.SelectCharacter(selected_character);
-      }
+    }
+    else if (_mouse_was_down)
+    {
+      _mouse_was_down = false;
     }
 	}
 }
