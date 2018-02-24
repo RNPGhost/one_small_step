1788d775a257cd8713fd0898d241b6f71c58ac2d
Unknown <samhutton@hotmail.co.uk>
2018-02-24 13:52:05 +0000

    Move world check into InputManager

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -4,19 +4,22 @@ public class InputController : MonoBehaviour {
   [SerializeField]
   private Player _player;
   [SerializeField]
+  private World _world;
+  [SerializeField]
   private UIController _ui_controller;
   
   void Update () {
 		if (Input.GetMouseButtonDown(0)) {
       Vector3 mouse_position = Input.mousePosition;
       AbilityButton selected_button;
+      Character selected_character;
       if (_ui_controller.TrySelectAbilityButton(mouse_position, out selected_button))
       {
         _player.SelectAbility(selected_button.GetAbility());
       }
-      else
+      else if (_world.TrySelectCharacter(mouse_position, out selected_character))
       {
-        _player.TrySelectCharacter(mouse_position);
+        _player.SelectCharacter(selected_character);
       }
     }
 	}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,10 +3,6 @@
 public class Player : MonoBehaviour {
   [SerializeField]
   private string _id;
-  [SerializeField]
-  private World _world;
-  [SerializeField]
-  private UIController _ui_controller;
 
   private Ability _selected_ability = null;
 
@@ -23,13 +19,12 @@ public class Player : MonoBehaviour {
     }
   }
 
-  public bool TrySelectCharacter(Vector3 mouse_position) {
-    Character selected_character;
-    if (_selected_ability != null && _world.TrySelectCharacter(mouse_position, out selected_character)) {
-      _selected_ability.SelectTarget(selected_character, out _selected_ability);
-      return true;
+  public bool SelectCharacter(Character character)
+  {
+    if (_selected_ability != null)
+    {
+      return _selected_ability.SelectTarget(character, out _selected_ability);
     }
-
     return false;
   }
 }
