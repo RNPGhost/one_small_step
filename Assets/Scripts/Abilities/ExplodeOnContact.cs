5941dbee6056a90ce48d2894f3c6feb7a3dbc4fd
Unknown <samhutton@hotmail.co.uk>
2018-02-27 21:09:30 +0000

    Make fireball deal damage and get destroyed on contact

---
+++ Assets/Scripts/Abilities/ExplodeOnContact.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeOnContact : MonoBehaviour {
+  [SerializeField]
+  private float _damage;
+
+  private void OnTriggerEnter(Collider other)
+  {
+    Character target = other.gameObject.GetComponent<Character>();
+    if (target != null)
+    {
+      target.TakeDamage(_damage);
+    }
+    Destroy(gameObject);
+  }
+}
