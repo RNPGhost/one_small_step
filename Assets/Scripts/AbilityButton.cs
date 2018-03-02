b875fd9b1c2d831300224052801ce1e4ce0b8fcd
Sam <sfh@corefiling.com>
2018-03-02 15:39:46 +0000

    Grey out ability buttons when the owning character is busy

--- Assets/Scripts/AbilityButton.cs
+++ Assets/Scripts/AbilityButton.cs
@@ -7,11 +7,18 @@ public class AbilityButton : MonoBehaviour {
   [SerializeField]
   private Text _text;
 
-  private void Start() {
-    _text.text = _ability.GetName();
-  }
+  private Button _button;
 
   public Ability GetAbility() {
     return _ability;
+  }
+
+  private void Start() {
+    _text.text = _ability.GetName();
+    _button = gameObject.GetComponent<Button>();
+  }
+
+  private void Update() {
+    _button.interactable = _ability.OwningCharacter.IsReadyToActivateAbility();
   }
 }
