44e15d8bafc88e8f1f99966a8825ea0f63a8f62f
Unknown <samhutton@hotmail.co.uk>
2018-03-06 14:10:21 +0000

    Add new sweep ability script

---
+++ Assets/Scripts/Abilities/SweepAbility.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+
+public class SweepAbility : Ability
+{
+  private Character _selected_character;
+  private Character _target;
+
+  public override string GetName()
+  {
+    return "Sweep";
+  }
+
+  public override float GetEnergyCost()
+  {
+    return 40f;
+  }
+
+  public override void SetTarget(Character character)
+  {
+    if (!IsInProgress() && IsValidTarget(character))
+    {
+      _selected_character = character;
+    }
+  }
+
+  public override void Reset()
+  {
+    if (!IsInProgress())
+    {
+      _selected_character = null;
+    }
+  }
+
+  protected override bool IsReadyToBeActivated()
+  {
+    return base.IsReadyToBeActivated() && _selected_character != null;
+  }
+
+  protected override void AbilitySpecificPhaseUpdate(Phase phase)
+  {
+    switch (phase.Name)
+    {
+      case PhaseName.Ready:
+        OwningCharacter.UnsetDirectionTarget(_target);
+        break;
+      case PhaseName.Preparation:
+        _target = _selected_character.AcquireAsTargetBy(OwningCharacter);
+        OwningCharacter.SetDirectionTarget(_target);
+        break;
+      case PhaseName.Recovery:
+        _target.Interrupt();
+        break;
+      default:
+        break;
+    }
+  }
+
+  private void Start()
+  {
+    SetPhases(new Phase[] {
+      new Phase(PhaseName.Ready),
+      new Phase(PhaseName.Preparation, 0.6f),
+      new Phase(PhaseName.Recovery, 1.5f),
+      });
+  }
+
+  private bool IsValidTarget(Character character)
+  {
+    return (character.OwningPlayer.Id != OwningCharacter.OwningPlayer.Id
+            && character.Targetable);
+  }
+}
