ce2ed56d37c1229811156542d146761abce8b037
Unknown <samhutton@hotmail.co.uk>
2018-03-06 22:59:19 +0000

    Update the ability button cooldown bar

--- Assets/Scripts/UI/AbilityButton.cs
+++ Assets/Scripts/UI/AbilityButton.cs
@@ -6,6 +6,8 @@ public class AbilityButton : MonoBehaviour {
   private Ability _ability;
   [SerializeField]
   private Text _text;
+  [SerializeField]
+  private Slider _cooldown_bar;
 
   private Button _button;
 
@@ -19,6 +21,28 @@ public class AbilityButton : MonoBehaviour {
   }
 
   private void Update() {
-    _button.interactable = !_ability.IsInProgress() && _ability.OwningCharacter.IsReadyToActivateAbility(_ability);
+    bool can_select_ability = !_ability.IsInProgress() && _ability.OwningCharacter.IsReadyToActivateAbility(_ability);
+    _button.interactable = can_select_ability;
+    _cooldown_bar.gameObject.SetActive(!can_select_ability);
+    UpdateCooldownBarValue();
+  }
+
+  private void UpdateCooldownBarValue()
+  {
+    float energy_progress = _ability.OwningCharacter.Energy / _ability.GetEnergyCost();
+    float ability_progress = GetAbilityProgress(_ability);
+    Ability active_ability = _ability.OwningCharacter.ActiveAbility;
+    float active_ability_progress = 1f;
+    if (active_ability != null)
+    {
+      active_ability_progress = GetAbilityProgress(_ability.OwningCharacter.ActiveAbility);
+    }
+    _cooldown_bar.value = Mathf.Min(energy_progress, ability_progress, active_ability_progress);
+  }
+
+  private float GetAbilityProgress(Ability ability)
+  {
+    float total_ability_time = ability.GetTotalAbilityTime();
+    return (total_ability_time - ability.GetRemainingTime()) / total_ability_time;
   }
 }
