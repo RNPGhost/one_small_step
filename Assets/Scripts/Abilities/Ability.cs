d16962fa0a3e33302216f641b0269899ee6b15ba
Unknown <samhutton@hotmail.co.uk>
2018-03-06 18:41:23 +0000

    Run a phase update after being interupted

--- Assets/Scripts/Abilities/Ability.cs
+++ Assets/Scripts/Abilities/Ability.cs
@@ -53,6 +53,7 @@ public abstract class Ability : MonoBehaviour {
     if (Interruptable()) {
       PausePhaseTransition();
       SetToNextReadyPhase();
+      PhaseUpdate(_phases.Current());
       return true;
     }
 
