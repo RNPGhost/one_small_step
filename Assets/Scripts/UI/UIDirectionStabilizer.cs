97e4843e8c3c8ba4ac3f6de5bb73da58a03f31af
Unknown <samhutton@hotmail.co.uk>
2018-03-06 09:14:42 +0000

    Health and energy bars always face away from the camera

---
+++ Assets/Scripts/UI/UIDirectionStabilizer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIDirectionStabilizer : MonoBehaviour {
+
+  [SerializeField]
+  private Camera _camera;
+
+  private Quaternion _rotation;
+
+  private void Start()
+  {
+    Vector3 look_direction = new Vector3(0, 0, _camera.transform.forward.z);
+    transform.rotation = Quaternion.LookRotation(look_direction);
+    _rotation = transform.rotation;
+  }
+
+  private void LateUpdate()
+  {
+    transform.rotation = _rotation;
+  }
+}
