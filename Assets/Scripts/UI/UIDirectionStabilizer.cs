102131b9a284950a45b09f0278e2bf189b50e6f4
Unknown <samhutton@hotmail.co.uk>
2018-03-06 18:48:46 +0000

    Find camera component independently rather than relying on a reference

--- Assets/Scripts/UI/UIDirectionStabilizer.cs
+++ Assets/Scripts/UI/UIDirectionStabilizer.cs
@@ -2,14 +2,12 @@
 
 public class UIDirectionStabilizer : MonoBehaviour {
   
-  [SerializeField]
-  private Camera _camera;
-
   private Quaternion _rotation;
 
   private void Start()
   {
-    Vector3 look_direction = new Vector3(0, 0, _camera.transform.forward.z);
+    Camera camera = FindObjectOfType<Camera>();
+    Vector3 look_direction = new Vector3(0, 0, camera.transform.forward.z);
     transform.rotation = Quaternion.LookRotation(look_direction);
     _rotation = transform.rotation;
   }
